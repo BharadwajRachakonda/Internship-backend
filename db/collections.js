2061d32a218abd48d3279530ea358e768a8ae605
Bharadwaj <rbharadwaj022@gmail.com>
2025-08-24 19:47:09 +0530

    adding description to items

--- db/collections.js
+++ db/collections.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const item = new Schema({
   name: { type: String, required: true },
   cost: { type: Number, required: true },
+  description: { type: String, required: true },
   imageURL: { type: String, required: true },
 });
 
